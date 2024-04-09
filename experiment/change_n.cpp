faaa2e615fef5b91041d4dd0dfca46cab966cb70
zkou <zkouaaa@gmail.com>
2024-04-09 18:36:44 +0900

    add sdsl new rmq to change n scripts

--- experiment/change_n.cpp
+++ experiment/change_n.cpp
@@ -2,11 +2,12 @@
 
 #include "RMQRMM64.h"
 #include "hyperrmq/hyper_rmq.hpp"
-#include "hyperrmq/memutil.hpp"
+#include "sdsl/memory_management.hpp"
 #include "sdsl/rmq_support.hpp"
 
 using namespace std;
 using namespace average_case_optimal_rmq;
+using namespace sdsl;
 
 using HyperRMQHuffman =
     HyperRMQ<16, CompressedMicrotreeSplitRankArrayHuffman<16>>;
@@ -57,6 +58,51 @@ void benchmarkFerrada(const vector<int>& perm,
                       const vector<pair<int, int>>& queries) {
     cout << "FerradaRMQ" << endl;
 
+    const int N = perm.size();
+
+    vector<pair<string, double>> benchmark_result;
+    auto start = chrono::system_clock::now();
+    auto timer = [&]() -> double {
+        auto now = chrono::system_clock::now();
+        return static_cast<double>(
+            chrono::duration_cast<chrono::microseconds>(now - start).count() /
+            1000.0);
+    };
+    auto time_keeper = [&](string message, bool end) -> void {
+        if (end) {
+            benchmark_result.push_back({message, timer()});
+        } else {
+            start = chrono::system_clock::now();
+        }
+    };
+
+    auto perm_reverse = new int[N];
+    copy(perm.rbegin(), perm.rend(), perm_reverse);
+
+    time_keeper("construction", 0);
+    RMQRMM64 rmq(perm_reverse, N);
+    time_keeper("construction", 1);
+
+    time_keeper("query", 0);
+    for (auto&& [i, j] : queries) {
+        rmq.queryRMQ(N - 1 - j, N - 1 - i);
+    }
+    time_keeper("query", 1);
+
+    benchmark_result.push_back({"memory", 8.0 * rmq.getSize() / perm.size()});
+
+    for (auto&& [label, value] : benchmark_result) {
+        cout << left << setw(20) << (label + ":") << right << setw(20) << value
+             << endl;
+    }
+
+    delete[] perm_reverse;
+}
+
+void benchmarkSDSLNEW(const vector<int>& perm,
+                      const vector<pair<int, int>>& queries) {
+    cout << "SDSLNEWRMQ" << endl;
+
     vector<pair<string, double>> benchmark_result;
     auto start = chrono::system_clock::now();
     auto timer = [&]() -> double {
@@ -74,62 +120,23 @@ void benchmarkFerrada(const vector<int>& perm,
     };
 
     time_keeper("construction", 0);
-    RMQRMM64 rmq(const_cast<int*>(perm.data()), perm.size());
+    rmq_succinct_rec_new<true, 2048, 1024, 128, 0> rmq(&perm);
     time_keeper("construction", 1);
 
     time_keeper("query", 0);
     for (auto&& [i, j] : queries) {
-        rmq.queryRMQ(i, j);
+        rmq(i, j);
     }
     time_keeper("query", 1);
 
-    benchmark_result.push_back({"memory", 8.0 * rmq.getSize() / perm.size()});
+    benchmark_result.push_back(
+        {"memory", 8.0 * size_in_bytes(rmq) / perm.size()});
 
     for (auto&& [label, value] : benchmark_result) {
         cout << left << setw(20) << (label + ":") << right << setw(20) << value
              << endl;
     }
 }
-
-// void benchmarkSDSL(const vector<int>& perm,
-//                    const vector<pair<int, int>>& queries) {
-//     cout << "SDSLRMQ" << endl;
-
-//     vector<pair<string, double>> benchmark_result;
-//     auto start = chrono::system_clock::now();
-//     auto timer = [&]() -> double {
-//         auto now = chrono::system_clock::now();
-//         return static_cast<double>(
-//             chrono::duration_cast<chrono::microseconds>(now - start).count()
-//             / 1000.0);
-//     };
-//     auto time_keeper = [&](string message, bool end) -> void {
-//         if (end) {
-//             benchmark_result.push_back({message, timer()});
-//         } else {
-//             start = chrono::system_clock::now();
-//         }
-//     };
-
-//     time_keeper("construction", 0);
-//     sdsl::rmq rmq(perm.data(), perm.size());
-//     time_keeper("construction", 1);
-
-//     time_keeper("query", 0);
-//     for (auto&& [i, j] : queries) {
-//         rmq.queryRMQ(i, j);
-//     }
-//     time_keeper("query", 1);
-
-//     benchmark_result.push_back({"memory", 8.0 * rmq.getSize() /
-//     perm.size()});
-
-//     for (auto&& [label, value] : benchmark_result) {
-//         cout << left << setw(20) << (label + ":") << right << setw(20) <<
-//         value
-//              << endl;
-//     }
-// }
 
 int main() {
     cout << fixed << setprecision(6);
@@ -159,6 +166,8 @@ int main() {
         }
 
         benchmarkFerrada(perm, queries);
+
+        benchmarkSDSLNEW(perm, queries);
 
         benchmarkHyperRMQ<HyperRMQHuffman>(
             perm, B_huf, queries, "HyperRMQHuffman B = " + to_string(B_huf));
