4fa951c4e9b2c7dba055df48e97375e23ee481f0
zkou <zkouaaa@gmail.com>
2024-04-16 14:14:33 +0900

    add change W scripts

---
+++ experiment/change_W_Huffman.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+
+#include "hyperrmq/hyper_rmq.hpp"
+#include "hyperrmq/memutil.hpp"
+
+using namespace std;
+using namespace hyperrmq;
+
+void output_benchmark(
+    pair<vector<pair<string, uint64_t>>, vector<pair<string, int>>> bm_result) {
+    auto [memory_result, time_result] = bm_result;
+    cout << "memory:" << endl;
+    for (auto&& [label, val] : memory_result) {
+        cout << left << setw(25) << (label + ":") << right << setw(20) << val
+             << endl;
+    }
+
+    cout << "time:" << endl;
+    for (auto&& [label, val] : time_result) {
+        cout << setw(20) << label << ":" << setw(20) << val << " ms" << endl;
+    }
+}
+
+template <typename RMQ>
+void benchmark(const vector<int>& perm, int B,
+               const vector<pair<int, int>>& queries) {
+    cout << typeid(RMQ).name() << endl;
+    cout << "B = " << B << endl;
+
+    vector<pair<string, int>> time_result;
+    auto start = chrono::system_clock::now();
+    auto timer = [&]() -> int {
+        auto now = chrono::system_clock::now();
+        return static_cast<double>(
+            chrono::duration_cast<chrono::microseconds>(now - start).count() /
+            1000.0);
+    };
+    auto time_keeper = [&](string message, bool end) -> void {
+        if (end) {
+            time_result.push_back({message, timer()});
+        } else {
+            start = chrono::system_clock::now();
+        }
+    };
+
+    time_keeper("construction", 0);
+    RMQ rmq(perm, B);
+    time_keeper("construction", 1);
+
+    time_keeper("query", 0);
+    for (auto&& [i, j] : queries) {
+        rmq.query(i, j);
+    }
+    time_keeper("query", 1);
+
+    vector<pair<string, uint64_t>> memory_result = rmq.memory_table();
+
+    output_benchmark({memory_result, time_result});
+};
+
+template <int i, int j>
+struct bm {
+    bm(const vector<int>& perm, int B, const vector<pair<int, int>>& queries) {
+        constexpr int si = 1 << i;
+        constexpr int sj = 1 << j;
+        cout << si << " " << sj << endl;
+        benchmark<HyperRMQ<si, CompressedMicrotreeSplitRankArrayHuffman<sj>>>(
+            perm, B, queries);
+    }
+};
+
+int main() {
+    cout << fixed << setprecision(5);
+
+    constexpr int N = 1e6, Q = 1e4;
+
+    auto timeinfo =
+        chrono::system_clock::to_time_t(chrono::system_clock::now());
+    cout << std::ctime(&timeinfo) << endl;
+    cout << "N = " << N << endl;
+    cout << "Q = " << Q << endl;
+
+    mt19937 engine(0);
+    vector<int32_t> perm(N);
+    iota(perm.begin(), perm.end(), 0);
+    shuffle(perm.begin(), perm.end(), engine);
+
+    vector<pair<int, int>> queries(Q);
+    for (int i = 0; i < Q; i++) {
+        queries[i] = {engine() % N, engine() % N};
+        if (queries[i].first > queries[i].second) {
+            swap(queries[i].first, queries[i].second);
+        }
+    }
+
+    const int B = 8;
+
+    bm<0, 0>(perm, B, queries);
+    bm<1, 1>(perm, B, queries);
+    bm<2, 2>(perm, B, queries);
+    bm<3, 3>(perm, B, queries);
+    bm<4, 4>(perm, B, queries);
+    bm<5, 5>(perm, B, queries);
+    bm<6, 6>(perm, B, queries);
+    bm<7, 7>(perm, B, queries);
+    bm<8, 8>(perm, B, queries);
+
+    return 0;
+}
---
+++ result/change_W_Huffman.py
@@ -0,0 +1,80 @@
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+for_paper = False
+
+
+py_name = sys.argv[0]
+txt_name = py_name.replace(".py", ".txt")
+fig_name = py_name.replace(".py", ".pdf" if for_paper else ".png")
+
+plt.rcParams["font.size"] = 22
+if for_paper:
+    plt.rcParams["text.usetex"] = True
+    plt.rcParams["font.family"] = "Times New Roman"
+
+
+with open(txt_name) as f:
+    lines = f.readlines()
+
+
+print(lines.pop(0).rstrip())  # date
+lines.pop(0)
+n = int(lines.pop(0).split()[-1])  # n
+q = int(lines.pop(0).split()[-1])  # q
+
+
+step = 19
+
+
+def lines_to_np(lines, start, step, index=-1):
+    return np.array(
+        list(map(lambda line: int(line.split()[index]), lines[start::step]))
+    )
+
+
+Ws = lines_to_np(lines, 0, step)
+total = lines_to_np(lines, 4, step)
+query = lines_to_np(lines, 18, step, -2)
+
+total = total / n
+query = query / q * 1e3
+
+necessary_indices = [6, 10, 12]
+necessary_arr = 0
+for idx in necessary_indices:
+    necessary_arr += lines_to_np(lines, idx, step)
+
+necessary = necessary_arr[0]
+assert np.all(necessary == necessary_arr)
+print(necessary / n)
+
+fig, ax = plt.subplots()
+
+ax.set_xlabel("Space consumption (bpe)")
+ax.set_xlim(2, 5)
+ax.set_yscale("log")
+ax.set_ylabel("Average query time [$\mu$s]")
+ax.set_ylim(10 / 12, 12 * 100)
+ax.grid()
+ax.plot(total, query, marker="o")
+
+for i, (t, q, w) in enumerate(zip(total, query, Ws)):
+    shiftx = 0.05
+    prody = 1.1 ** (1 - i / 7)
+    if w == 256:
+        ax.text(t + shiftx, q * prody, str(w), ha="left", va="top")
+    elif w >= 32:
+        ax.text(t + shiftx, q * prody, str(w), ha="left", va="center")
+    else:
+        ax.text(t + shiftx, q * prody, str(w), ha="left", va="bottom")
+
+
+ax.axvline(necessary / n, linestyle="dotted")
+# ax.set_xlabel("Tree-covering parameter $B$")
+# ax.set_ylabel("Space consumption (bpe)")
+
+plt.tight_layout()
+plt.savefig(fig_name)
